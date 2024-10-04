4bb4ae18ae05196655d5ac8c5a2e1ddc2a2fe753
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-04 17:41:23 +0300

    updated

---
+++ js/task-02.js
@@ -0,0 +1,21 @@
+// const ingredients = [
+//   'Potatoes',
+//   'Mushrooms',
+//   'Garlic',
+//   'Tomatos',
+//   'Herbs',
+//   'Condiments',
+// ];
+
+// // const option = ingredients[0];
+// // console.log(option);
+
+// const element = ingredients.map(option{})
+// const creatingElement = document.createElement('li');
+// creatingElement.textContent = option;
+// creatingElement.classList.add('item')
+
+// console.log(creatingElement);
+// // const creatingList = element => {
+
+// // };
