8bbcca026c3cf24723aaa549e034aa27c1720a90
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-09 12:21:11 +0300

    option 2

--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,33 @@
+
+// // // Option 1
+
+// const nameInputBtn = document.querySelector('#name-input');
+// const nameOutputBtn = document.querySelector('#name-output');
+
+// nameInputBtn.addEventListener('input', callback);
+
+// function callback(el) {
+//     nameOutputBtn.textContent = el.currentTarget.value;
+
+//     if ( el.currentTarget.value === '') {
+//         nameOutputBtn.textContent = 'Anonymous'; 
+//     } else {
+//           nameOutputBtn.textContent = el.currentTarget.value;
+//     }
+// }
+
+
+// // Option 2
+
 const nameInputBtn = document.querySelector('#name-input');
 const nameOutputBtn = document.querySelector('#name-output');
 
 nameInputBtn.addEventListener('input', () => {
     const inputValue = nameInputBtn.value;
 
-
-
-    if (inputValue === "") {
-        nameOutputBtn.textContent = 'Anonymous';
+    if (inputValue === '') {
+       nameOutputBtn.textContent = 'Anonymous' 
     } else {
         nameOutputBtn.textContent = inputValue;
     }
-});
+})
