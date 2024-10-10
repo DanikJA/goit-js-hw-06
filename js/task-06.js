73823c46bf484e1536c14e8b3e104493480a63e9
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-10 12:16:31 +0300

    problem solving

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,61 @@
-const input = document.getElementById('validation-input');
+const valInput = document.querySelector('#validation-input');
 
-    input.addEventListener('blur', () => {
-        const requiredLength = parseInt(input.getAttribute('data-length'), 10);
-        const currentLength = input.value.length;
 
-        if (currentLength === requiredLength) {
-            input.classList.remove('invalid');
-            input.classList.add('valid');
-        } else {
-            input.classList.remove('valid');
-            input.classList.add('invalid');
-        }
-    });
+valInput.addEventListener('blur', (event) => {
+    const correctLength = parseInt(valInput.getAttribute('data-length'), 10)
+    const currentLength = event.currentTarget.value.length;
+
+    if (currentLength === correctLength) {
+        valInput.classList.remove('invalid');
+        valInput.classList.add('valid');
+    } else {
+        valInput.classList.add('invalid');
+        valInput.classList.remove('invalid');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const input = document.getElementById('validation-input');
+
+//     input.addEventListener('blur', () => {
+//         const requiredLength = parseInt(input.getAttribute('data-length'), 10);
+//         const currentLength = input.value.length;
+
+//         if (currentLength === requiredLength) {
+//             input.classList.remove('invalid');
+//             input.classList.add('valid');
+//         } else {
+//             input.classList.remove('valid');
+//             input.classList.add('invalid');
+//         }
+//     });
+
+
+//     const myInput = document.querySelector("#validation-input");
+// myInput.addEventListener("blur", onBlur);
+// function onBlur(e) {
+//   const input1 = e.currentTarget;
+//   const inputLength = Number(input1.dataset.length);
+//   if (input1.value.length === inputLength) {
+//     input1.classList.remove("invalid");
+//     input1.classList.add("valid");
+//   } else {
+//     input1.classList.remove("valid");
+//     input1.classList.add("invalid");
+//   }
+//   //console.log(input1);
+//   //console.log(inputLength);
+// }
