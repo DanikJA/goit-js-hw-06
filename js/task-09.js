249e43dd1b0efad440d9032e17c63c4a9f65c5da
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-12 13:25:36 +0300

    created script for changing color by using random color

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const colorEl = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+
+changeColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+
+  bodyEl.style.background = randomColor;
+  colorEl.textContent = randomColor;
+});
+
