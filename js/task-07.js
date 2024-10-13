6e61eb6abfa05bfb528320e96447e57892a24926
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-13 20:37:41 +0300

    comit

--- js/task-07.js
+++ js/task-07.js
@@ -3,7 +3,6 @@ const spanEl = document.querySelector('#text');
     inputFontSizeControl.value = 16;
     spanEl.style.fontSize = "16px"
 
-
 const callback = () => {
     spanEl.style.fontSize = `${inputFontSizeControl.value}px`
 }
